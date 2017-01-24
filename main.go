3d8f773220afe9d67d82d9061632a58dffb11e2c
Cesar Gimenes <crg@crg.eti.br>
2017-01-24 13:08:08 -0200

    simple url encode

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+	"io/ioutil"
+
+	"github.com/crgimenes/goConfig"
+)
+
+type config struct {
+	File string `cfg:"file"`
+}
+
+func main() {
+	cfg := config{}
+
+	goConfig.PrefixEnv = "BASE64"
+	err := goConfig.Parse(&cfg)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	if cfg.File == "" {
+		goConfig.Usage()
+		return
+	}
+
+	buff, err := ioutil.ReadFile(cfg.File)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	encoded := base64.StdEncoding.EncodeToString(buff)
+
+	fmt.Println(encoded)
+
+}
